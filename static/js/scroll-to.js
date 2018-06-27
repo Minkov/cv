284025eff6fb48f56fdcec9ebaf39046db0d937e
Doncho Minkov <donchominkov@gmail.com>
2018-06-27 20:49:36 +0300

    UI Fixes

--- static/js/scroll-to.js
+++ static/js/scroll-to.js
@@ -1,7 +1,6 @@
 $(function () {
     $(".btn-scroll").on("click", function (ev) {
         var el = $(this).attr("href");
-        console.log(el);
         $('body').scrollTo($(el), {
             duration: 1000
         });
